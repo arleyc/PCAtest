ba53d0a671215911dea8a50c76399d5fe6c74f5f
Arley Camargo <arley.camargo@gmail.com>
2021-09-08 15:18:12 -0300

    new commit

--- R/PCAtest.R
+++ R/PCAtest.R
@@ -71,22 +71,22 @@
 #' @examples
 #'#PCA analysis of five uncorrelated variables
 #'data("ex0")
-#'result<-PCAtest(ex0, 100, 100, 0.05, varcorr=FALSE, plot=TRUE)
+#'result<-PCAtest(ex0, 100, 100, 0.05, varcorr=FALSE, counter=FALSE, plot=TRUE)
 #'
 #'#PCA analysis of five correlated (r=0.5) variables
 #'data("ex05")
-#'result<-PCAtest(ex05, 100, 100, 0.05, varcorr=FALSE, plot=TRUE)
+#'result<-PCAtest(ex05, 100, 100, 0.05, varcorr=FALSE, counter=FALSE, plot=TRUE)
 #'
 #'#PCA analysis of five correlated (r=1) variables
 #'v1<-seq(0,1,0.01)
 #'v5=v4=v3=v2=v1
 #'ex1<-cbind(v1,v2,v3,v4,v5)
-#'result<-PCAtest(ex1, 100, 100, 0.05, varcorr=FALSE, plot=TRUE)
+#'result<-PCAtest(ex1, 100, 100, 0.05, varcorr=FALSE, counter=FALSE, plot=TRUE)
 #'
 #'#PCA analysis of seven morphological variables from 29 ant species (from
 #'#Wong and Carmona 2021, https://doi.org/10.1111/2041-210X.13568)
 #'data("ants")
-#'result<-PCAtest(ants, 100, 100, 0.05, varcorr=FALSE, plot=TRUE)
+#'result<-PCAtest(ants, 100, 100, 0.05, varcorr=FALSE, counter=FALSE, plot=TRUE)
 
 PCAtest <- function(x, nperm=1000, nboot=1000, alpha=0.05, indload=T, varcorr=F, counter=T, plot=T) {
 
